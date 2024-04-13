81495ed08b91f038ba88814e94990754813b7601
João Henrique Terêncio <94974380+Jh-Terencio@users.noreply.github.com>
2024-04-12 21:55:37 -0300

    learning about aplly

--- aula-10-apply.r
+++ aula-10-apply.r
@@ -2,6 +2,9 @@ weight <- c(60, 72, 57, 90, 95, 72)
 height <- c(1.75, 1.80, 1.65, 1.90, 1.74, 1.91)
 subject <- c("A", "B", "C", "D", "E", "F")
 
+
+# apply() takes Data frame or matrix as an input and gives output in vector, list or array. Apply function in R is primarily used to avoid explicit uses of loop constructs. It is the most basic of all collections can be used over a matrices.
+
 df <- data.frame(weight=weight, height=height, subject=subject)
 head(df)
 
